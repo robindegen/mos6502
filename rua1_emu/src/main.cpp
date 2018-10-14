61578991c907ad58cb5bf71307905f8dc1dcad80
Robin Degen <robindegen@gmail.com>
2018-10-14 14:33:57 +0200

    Instantiate the cpu so that some initial tests can be done.

--- rua1_emu/src/main.cpp
+++ rua1_emu/src/main.cpp
@@ -1,6 +1,16 @@
 #include <mos6502/cpu_mos6502.h>
 
+void bus_write(const std::uint16_t, const std::uint8_t) noexcept
+{
+}
+
+auto bus_read(const std::uint16_t) noexcept -> std::uint8_t
+{
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
+    mos6502::cpu_mos6502 cpu{bus_read, bus_write};
     return 0;
 }
